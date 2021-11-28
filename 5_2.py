f9c5b6009e90b779e23e8e89693496ea92c74411
lyuda <jiminni_pabo@mail.ru>
2021-11-28 18:51:34 +0300

    lesson 5 task 2

---
+++ 5_2.py
@@ -0,0 +1,12 @@
+odd_num = 15
+odd_to_15 = (n for n in range(1, odd_num + 1, 2))
+
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
